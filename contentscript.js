443e970b73f301ffd7abce4ca6b716f166c196e6
Anto <antonybharath.mariadurairaj@chargebackgurus.com>
2023-05-17 16:58:05 +0530

    chnages

--- contentscript.js
+++ contentscript.js
@@ -1,34 +1,38 @@
 
-  var inputs=document.getElementsByTagName("input");
+//   var inputs=document.getElementsByTagName("input");
 
-  id = e => { return e.name || e.id || e.innerText || e.placeholder.replace(/\s/g, '_'); };
+//   id = e => { return e.name || e.id || e.innerText || e.placeholder.replace(/\s/g, '_'); };
 
-  for(var i=0;i<inputs.length;i++){
-    var input=inputs[i];
-    console.info(input)
-    if(input.type=="password"){
-      {input.value="Password@123"} 
-      // keydownupFunc(input.value)
-    }
-    if(input.type=="text" || input.type =="email" || input.type =="number"){
-      {input.value="weuio"}
-      // keydownupFunc(input.value);
-    }
-    if(input.type  && input.type == "submit"){
-      input.click();    
-      console.log()
-    };
-  }
+//   for(var i=0;i<inputs.length;i++){
+//     var input=inputs[i];
+//     console.info(input)
+//     if(input.type=="password"){
+//       {input.value="Password@123"} 
+//       // keydownupFunc(input.value)
+//     }
+//     if(input.type=="text" || input.type =="email" || input.type =="number"){
+//       {input.value="weuio"}
+//       // keydownupFunc(input.value);
+//     }
+//     if(input.type  && input.type == "submit"){
+//       input.click();
+//       if(input.formNoValidate === false){
+//         input.value = ""
+//         break;
+//       }    
+//       console.log(input)
+//     };
+//   }
 
-  var btnElement = document.getElementsByTagName("button");
-for(const ele of btnElement){
-   var expVal = new RegExp('(?:login)|(?:sign)|(?:Sign)|(?:submit)|(?:loginform)|(?:continue)|(?:next)', 'i');              
-   const name = id(ele);
-    if(expVal.test(name)){
-      console.log(name)
-     ele.click();
-   }
-  }
+//   var btnElement = document.getElementsByTagName("button");
+// for(const ele of btnElement){
+//    var expVal = new RegExp('(?:login)|(?:sign)|(?:Sign)|(?:submit)|(?:loginform)|(?:continue)|(?:next)', 'i');              
+//    const name = id(ele);
+//     if(expVal.test(name)){
+//       console.log(name)
+//      ele.click();
+//    }
+//   }
 
   //   function keydownupFunc(value) {
     
--- popup.js
+++ popup.js
@@ -16,7 +16,41 @@
 //   }
 // }
 
+function changeBackgroundColor() {
+   var inputs=document.getElementsByTagName("input");
 
+  id = e => { return e.name || e.id || e.innerText || e.placeholder.replace(/\s/g, '_'); };
+
+  for(var i=0;i<inputs.length;i++){
+    var input=inputs[i];
+    console.info(input)
+    if(input.type=="password"){
+      {input.value="645e1ecbbd7a0"} 
+      // keydownupFunc(input.value)
+    }
+    if(input.type=="text" || input.type =="email" || input.type =="number"){
+      {input.value="antony@codeboardtech.com"}
+      // keydownupFunc(input.value);
+    }
+    if(input.type  && input.type == "submit"){
+      input.click();
+      if(input.formNoValidate === false){
+        break;
+      }    
+      console.log(input)
+    };
+  }
+
+  var btnElement = document.getElementsByTagName("button");
+for(const ele of btnElement){
+   var expVal = new RegExp('(?:login)|(?:sign)|(?:Sign)|(?:submit)|(?:loginform)|(?:continue)|(?:next)', 'i');              
+   const name = id(ele);
+    if(expVal.test(name)){
+      console.log(name)
+     ele.click();
+   }
+  }
+}
 
 async function updateUI() {
   var checkPageButton = document.getElementById('checkPage');
@@ -26,8 +60,8 @@ async function updateUI() {
         console.log('Attempting to inject script into tab:',tab);
         chrome.scripting.executeScript({
           target: {tabId: tab.id},
-          files: ["./contentscript.js"],
-      });
+          func : changeBackgroundColor,
+      }).then(() => console.log("injected a function"));;
     });
 
   });
